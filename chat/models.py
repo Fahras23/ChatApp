9624706c041619d8c2fde513a5b0ed81a0f62f1f
Fahras23 <2001krzysztoflyczak@gmail.com>
2024-01-25 11:54:12 +0100

    fixing 2fa

--- chat/models.py
+++ chat/models.py
@@ -5,12 +5,17 @@ from django.db import models
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import pyotp
 
 class Profile(models.Model):  
     user = models.OneToOneField(User,on_delete=models.CASCADE)  
     code = models.CharField(max_length=100,blank=False)
     def __str__(self):
         return self.user.username
+    def generate_otp_secret(self):
+        if not self.code:
+            self.code = pyotp.random_base32()
+            self.save()
 
 class Room(models.Model):
     name = models.CharField(max_length=100,blank=False)
