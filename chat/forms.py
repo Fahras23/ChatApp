8f993be63b6cf97755c7fb7efbceeadffe37f320
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-02-07 00:38:53 +0100

    add ws user add

--- chat/forms.py
+++ chat/forms.py
@@ -4,21 +4,26 @@ from django.contrib.auth.models import User
 from .models import Profile
 from django.contrib.auth.forms import AuthenticationForm
 
+
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ["username", "email", "password"]
+
     def __init__(self, *args, **kwargs):
         super(UserForm, self).__init__(*args, **kwargs)
 
-        for fieldname in ['username', 'email', 'password']:
+        for fieldname in ["username", "email", "password"]:
             self.fields[fieldname].help_text = None
+
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label="Username", max_length=63)
     password = forms.CharField(label="Password", widget=forms.PasswordInput)
-    code = forms.CharField(label='Code',max_length=100)
+    code = forms.CharField(label="Code", max_length=100)
+
 
 class OTPForm(forms.Form):
-    otp_code = forms.CharField(label='OTP Code', max_length=6)
+    otp_code = forms.CharField(label="OTP Code", max_length=6)
