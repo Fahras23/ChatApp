9d69274d1a2b41d046fa3a81d1d1fd0cff3d0165
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-02-10 16:31:54 +0100

    update

--- chat/urls.py
+++ chat/urls.py
@@ -6,9 +6,11 @@ from chat import views
 urlpatterns = [
     # Home
     path("", views.home, name="home"),
-    path("room/<int:id>/", views.room_view, name="room_view"),
     path("remove_room/<int:id>/", views.remove_room, name="remove_room"),
     path("create_room", views.create_room, name="create_room"),
+    # Rooms
+    path("room/<int:id>/", views.room_view, name="room_view"),
+    # Auth process
     path("register/", views.register_user, name="register-user"),
     path("login/", views.login_user, name="login-user"),
     path("logout/", LogoutView.as_view(), name="logout-user"),
