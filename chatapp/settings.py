43874942bfec63fc5957410e0631d79ad0378ee3
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-02-24 12:16:49 +0100

    add debug env

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -24,7 +24,7 @@ AUTH_PROFILE_MODULE = "YOURAPP.UserProfile"
 SECRET_KEY = "django-insecure-@gv--puoex2!-q3yk#k^q)3zr%hp-tmeu=hde9#o&=nqne+e04"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("REDIS_HOST")
 
 # settings.py (Django Channels and Redis setup)
 INSTALLED_APPS = [
