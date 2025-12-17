3c3c79078c8933321a3a89afd765dfa08e93bf5d
Krzysztof Lyczak <56490146+Fahras23@users.noreply.github.com>
2025-12-17 12:51:17 +0100

    Update settings.py

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -21,7 +21,7 @@ AUTH_PROFILE_MODULE = 'YOURAPP.UserProfile'
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "***REMOVED***"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
