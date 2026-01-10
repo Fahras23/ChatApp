eff3d1839ea4aabe86d8d92b676ec83e430fa027
Fahras23 <2001krzysztoflyczak@gmail.com>
2026-01-10 12:04:40 +0100

    remove wss

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path("wss/room/(?P<id>\d+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path("ws/room/(?P<id>\d+)/$", consumers.ChatConsumer.as_asgi()),
 ]
